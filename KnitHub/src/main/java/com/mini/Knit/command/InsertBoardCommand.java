e1a024c2e0d8d07ff651e7f77126cdac9a95b9e6
김윤지 <21615008@konyang.ac.kr>
2024-11-09 15:57:23 +0900

    글 등록, 파일 등록(두개씩 업로드됨 수정필요)

--- KnitHub/src/main/java/com/mini/Knit/command/InsertBoardCommand.java
+++ KnitHub/src/main/java/com/mini/Knit/command/InsertBoardCommand.java
@@ -9,10 +9,13 @@ public class InsertBoardCommand {
 	private String title;
 	@NotBlank(message = "내용을 입력하세요")
 	private String content;
+	private String fileName;
+
 	public InsertBoardCommand() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+
 	public InsertBoardCommand(String id, @NotBlank(message = "제목을 입력하세요") String title,
 			@NotBlank(message = "내용을 입력하세요") String content) {
 		super();
@@ -20,28 +23,42 @@ public class InsertBoardCommand {
 		this.title = title;
 		this.content = content;
 	}
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public String getContent() {
 		return content;
 	}
+
 	public void setContent(String content) {
 		this.content = content;
 	}
+
+	public String getFileName() { // Add the getter for the file name
+		return fileName;
+	}
+
+	public void setFileName(String fileName) { // Add the setter for the file name
+		this.fileName = fileName;
+	}
+
 	@Override
 	public String toString() {
 		return "insertBoardCommand [id=" + id + ", title=" + title + ", content=" + content + "]";
 	}
-	
-	
+
 }
--- KnitHub/src/main/java/com/mini/Knit/controller/BoardController.java
+++ KnitHub/src/main/java/com/mini/Knit/controller/BoardController.java
@@ -1,5 +1,6 @@
 package com.mini.Knit.controller;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.List;
@@ -13,7 +14,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartRequest;
 
 import com.mini.Knit.command.DelBoardCommand;
@@ -24,104 +27,96 @@ import com.mini.Knit.dtos.FileBoardDto;
 import com.mini.Knit.service.BoardService;
 import com.mini.Knit.service.FileService;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Part;
 
 @Controller
 @RequestMapping(value = "/board")
 public class BoardController {
-	
+
 	@Autowired
 	private BoardService boardService;
 	@Autowired
 	private FileService fileService;
-	
+
 	@GetMapping(value = "/boardList")
 	public String boardList(Model model) {
 		System.out.println("글목록 보기");
-		
-		List<BoardDto> list=boardService.getAllList();
+
+		List<BoardDto> list = boardService.getAllList();
 		model.addAttribute("list", list);
 		model.addAttribute("delBoardCommand", new DelBoardCommand());
 		return "board/boardList";// forward 기능, "redirect:board/boardList"
 	}
-	
+
 	@GetMapping(value = "/boardInsert")
 	public String boardInsertForm(Model model) {
 		model.addAttribute("insertBoardCommand", new InsertBoardCommand());
 		return "board/boardInsertForm";
 	}
-	
+
 	@PostMapping(value = "/boardInsert")
-	public String boardInsert(@Validated InsertBoardCommand insertBoardCommand 
-			                ,BindingResult result
-			                ,MultipartRequest multipartRequest //multipart data를 처리할때 사용
-							,HttpServletRequest request
-			                ,Model model) throws IllegalStateException, IOException {
-		if(result.hasErrors()) {
+	public String boardInsert(@Validated InsertBoardCommand insertBoardCommand, BindingResult result,
+			@RequestParam("filename") MultipartFile file, HttpServletRequest request, Model model)
+			throws IllegalStateException, IOException, ServletException {
+		if (result.hasErrors()) {
 			System.out.println("글을 모두 입력하세요");
 			return "board/boardInsertForm";
 		}
-		
-		boardService.insertBoard(insertBoardCommand,multipartRequest
-				                ,request);
-		
+
+// BoardService에 파일과 함께 게시글 추가 요청
+		boardService.insertBoard(insertBoardCommand, file, request);
+
 		return "redirect:/board/boardList";
 	}
-	
-	//상세보기
+
+	// 상세보기
 	@GetMapping(value = "/boardDetail")
 	public String boardDetail(int board_seq, Model model) {
-		BoardDto dto=boardService.getBoard(board_seq);
-		
-		//유효값처리용
+		BoardDto dto = boardService.getBoard(board_seq);
+
+		// 유효값처리용
 		model.addAttribute("updateBoardCommand", new UpdateBoardCommand());
-		//출력용
+		// 출력용
 		model.addAttribute("dto", dto);
 		System.out.println(dto);
 		return "board/boardDetail";
 	}
-	
-	//수정하기
+
+	// 수정하기
 	@PostMapping(value = "/boardUpdate")
-	public String boardUpdate(
-				@Validated UpdateBoardCommand updateBoardCommand
-				,BindingResult result
-				,Model model) {
-		
-		if(result.hasErrors()) {
+	public String boardUpdate(@Validated UpdateBoardCommand updateBoardCommand, BindingResult result, Model model) {
+
+		if (result.hasErrors()) {
 			System.out.println("수정내용을 모두 입력하세요");
-			//코드 추가--------------------------------------------
-			BoardDto dto=boardService.getBoard(updateBoardCommand.getBoard_seq());
+			// 코드 추가--------------------------------------------
+			BoardDto dto = boardService.getBoard(updateBoardCommand.getBoard_seq());
 			model.addAttribute("dto", dto);
-			//--------------------------------------------------
+			// --------------------------------------------------
 			return "board/boardDetail";
 		}
-		
+
 		boardService.updateBoard(updateBoardCommand);
-		
-		return "redirect:/board/boardDetail?board_seq="
-				+ updateBoardCommand.getBoard_seq();
+
+		return "redirect:/board/boardDetail?board_seq=" + updateBoardCommand.getBoard_seq();
 	}
-	
+
 	@GetMapping(value = "/download")
-	public void download(int file_seq, HttpServletRequest request
-			                         , HttpServletResponse response) throws UnsupportedEncodingException {
-		
-		FileBoardDto fdto=fileService.getFileInfo(file_seq);//파일정보가져오기
-		
-		fileService.fileDownload(fdto.getOrigin_filename()
-				                ,fdto.getStored_filename()
-				                ,request,response);
+	public void download(int file_seq, HttpServletRequest request, HttpServletResponse response)
+			throws UnsupportedEncodingException {
+
+		FileBoardDto fdto = fileService.getFileInfo(file_seq);// 파일정보가져오기
+
+		fileService.fileDownload(fdto.getOrigin_filename(), fdto.getStored_filename(), request, response);
 	}
-	
-	@RequestMapping(value="mulDel",method = {RequestMethod.POST,RequestMethod.GET})
-	public String mulDel(@Validated DelBoardCommand delBoardCommand
-						 ,BindingResult result
-			             , Model model) {
-		if(result.hasErrors()) {
+
+	@RequestMapping(value = "mulDel", method = { RequestMethod.POST, RequestMethod.GET })
+	public String mulDel(@Validated DelBoardCommand delBoardCommand, BindingResult result, Model model) {
+		if (result.hasErrors()) {
 			System.out.println("최소하나 체크하기");
-			List<BoardDto> list=boardService.getAllList();
+			List<BoardDto> list = boardService.getAllList();
 			model.addAttribute("list", list);
 			return "board/boardlist";
 		}
